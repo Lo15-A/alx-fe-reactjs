b34b053e86ffddbde94344474b860e2110917054
Lois Aideloje <loisaideloje@gmail.com>
2025-07-18 03:07:50 +0100

    Simple Counter App Using State and Hooks

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,16 +1,12 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+import React from 'react';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <>
-      <Header />
-      <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Footer />
-    </>
+    <div>
+      <h1>My React App</h1>
+      <Counter />
+    </div>
   );
 }
 
