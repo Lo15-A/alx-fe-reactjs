c035ae43b7050f04a9680235f8a78e74aada5d50
Lois Aideloje <loisaideloje@gmail.com>
2025-07-30 15:29:43 +0100

    Recipe Sharing Application with Zustand State Management

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,10 @@
+import { create } from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (recipe) =>
+    set((state) => ({ recipes: [...state.recipes, recipe] })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export default useRecipeStore;
