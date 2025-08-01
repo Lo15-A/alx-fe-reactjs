581576962a69523ee44641e89325674f3e261d2b
Lois Aideloje <loisaideloje@gmail.com>
2025-08-01 21:48:53 +0100

    Personalized Recipe Recommendations

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
+import FavouritesList from './components/FavouritesList';
+import RecommendationList from './components/RecommendationList';
 import RecipeDetails from './components/RecipeDetails';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <h1>Recipe Sharing App</h1>
-        <AddRecipeForm />
+        <h1>My Recipe App</h1>
+
+        {/* Main Routes */}
         <Routes>
           <Route path="/" element={<RecipeList />} />
+          <Route path="/favourites" element={<FavouritesList />} />
+          <Route path="/recommendations" element={<RecommendationList />} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
         </Routes>
       </div>
