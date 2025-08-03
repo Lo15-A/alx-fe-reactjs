0d44b114157b3055d3bca05b2d3a83d2aae78664
Lois Aideloje <loisaideloje@gmail.com>
2025-08-03 13:06:05 +0100

    Creating a Basic Search Feature

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react'; //  
+import React from 'react';
+import Search from './components/Search';
 
-function App() {
+const App = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold text-center text-blue-600">
-        GitHub User Search App
-      </h1>
+    <div>
+      <h1>GitHub User Search</h1>
+      <Search />
     </div>
   );
-}
+};
 
 export default App;
---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+};
