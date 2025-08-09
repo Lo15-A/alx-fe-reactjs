4fafbd90eedb194e26b3555c10a40d92dade6d01
Lois Aideloje <loisaideloje@gmail.com>
2025-08-09 20:05:49 +0100

    Setting Up Tailwind CSS in a New React Project

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-900">
+        Tailwind is working! 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
