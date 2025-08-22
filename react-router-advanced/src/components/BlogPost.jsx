0cb739f5e04e81305487fc15527e8545862ede93
Lois Aideloje <loisaideloje@gmail.com>
2025-08-22 23:29:06 +0100

    Advanced Routing in React with React Router

---
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -0,0 +1,16 @@
+// src/components/BlogPost.jsx
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function BlogPost() {
+  const { id } = useParams(); // dynamic parameter from the URL
+
+  return (
+    <div>
+      <h2>Blog Post #{id}</h2>
+      <p>This is the content for blog post with ID: {id}</p>
+    </div>
+  );
+}
+
+export default BlogPost;
